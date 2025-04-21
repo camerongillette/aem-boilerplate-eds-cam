5e504c88c71c66406c77b3514ffa4889603ef9c1
camerongillette <camerongillette@gmail.com>
2025-04-21 11:21:19 -0500

    Testimonial Component

---
+++ blocks/testimonial/testimonial.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+  [...block.children].forEach(row => {
+    const [quote, name, role, img] = row.children;
+    row.innerHTML = `
+      <div class="testimonial">
+        <img src="${img.textContent}" alt="${name.textContent}" />
+        <blockquote>${quote.textContent}</blockquote>
+        <p><strong>${name.textContent}</strong><br>${role.textContent}</p>
+      </div>
+    `;
+  });
+}
