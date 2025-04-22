807b31e67ec1e6c3d59dc3ffad013faafa20b4e6
camerongillette <camerongillette@gmail.com>
2025-04-22 13:18:13 -0500

    Cam File

--- blocks/camfile/camfile.js
+++ blocks/camfile/camfile.js
@@ -2,19 +2,19 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
- * loads and decorates the footer
- * @param {Element} block The footer block element
+ * loads and decorates the camFile
+ * @param {Element} block The camFile block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
+  // load camFile as fragment
   const camfileMeta = getMetadata('camfile');
   const camfilePath = camfileMeta ? new URL(camfileMeta, window.location).pathname : '/camfile';
   const fragment = await loadFragment(camfilePath);
 
-  // decorate footer DOM
+  // decorate camFile DOM
   block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  const camFile = document.createElement('div');
+  while (fragment.firstElementChild) camFile.append(fragment.firstElementChild);
 
-  block.append(footer);
+  block.append(camFile);
 }
